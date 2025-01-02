7197b592c977d7d1d1bdc674029e4145d5ae5377
robotsvision <robotsvision.vx@gmail.com>
2025-01-02 23:13:29 +0300

    [11:12]: Minimal initial commit. File vertex.hpp and license were changed:
    Vertex 5 Lina -> Vertex 5.

---
+++ include/vertex.hpp
@@ -0,0 +1,17 @@
+
+/**
+ * @file vertex.hpp
+ * @brief Main Vertex framework header file.
+ * @version 0.5.1
+ * @date 02.01.25
+ * @author Matvey Rybalkin
+ */
+
+#ifndef VERTEX_5_HPP_
+#define VERTEX_5_HPP_
+
+#if !defined(__cplusplus) || (__cplusplus < __cplusplus)
+#   error "<Vertex 5>: Requires at least the 2014 C++ language standard."
+#endif
+
+#endif // VERTEX_5_HPP_
